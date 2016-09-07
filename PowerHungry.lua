e3660bca945ef7cf6f341e0752138579a5963293
ZnatNO <dawgie@gmail.com>
2016-09-07 22:02:02 +0200

    Revert "zzZZ"
    This reverts commit 741386d8d0746c538d978e93b92eb8b9fa0ba889.

--- PowerHungry.lua
+++ PowerHungry.lua
@@ -61,7 +61,6 @@ btn:SetScript("OnEvent", function(_, event)
 				btn.itemID = id
 				btn:SetAttribute("item", "item:"..id)
 				tex:SetTexture(GetItemIcon(id))
-				fs:SetShown(false)
 				fs:SetText(TEXT_FMT:format(p))
 			else --fallback
 				btn:SetShown(false)
